e461736ee7b589c720146bc2b8f68c962dbba74c
Pedro Polignano Dias <pedroluanpd@gmail.com>
2020-12-10 16:17:37 -0300

    implementação dos métodos  Dividir, Multiplicar, Subtrair, Somar, MenuDeOpcoes, LigarCalculadora e PopularLista

--- ConsoleApp3/Calculadora.cs
+++ ConsoleApp3/Calculadora.cs
@@ -1,13 +1,17 @@
-﻿namespace Projeto
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace Projeto
 {
-    public class Calculadora
-    {
-        // Soma(double n1, double n2)
+     class Calculadora
+     {
         public double Soma(double n1, double n2)
         {
             return n1 + n2;
         }
-        public double Soma(params double[] numeros)
+
+        public double Soma(List<double> numeros)
         {
             double resultado = 0.0;
             foreach (double numero in numeros)
@@ -16,5 +20,43 @@
             }
             return resultado;
         }
-    }
+
+        public double Subtrai(List<double> numeros)
+        {
+            double primeiroNumero = numeros[0];
+            for (int i = 1; i < numeros.Count; i++)
+            {
+                primeiroNumero -= numeros[i];
+            }
+
+            return primeiroNumero;
+        }
+
+        public double Multiplica(List<double> numeros)
+        {
+            double primeiroNumero = numeros[0];
+            for (int i = 1; i < numeros.Count; i++)
+            {
+                primeiroNumero *= numeros[i];
+            }
+            return primeiroNumero;
+        }
+
+        public string Divide(List<double> numeros)
+        {
+            string resultado = "";
+            double primeiroNumero = numeros[0];
+            for (int i = 1; i < numeros.Count; i++)
+            {
+                if (numeros[i] == 0)
+                {
+                    resultado = "Impossível dividir por zero";
+                    return resultado;
+                }
+                primeiroNumero /= numeros[i];
+            }
+            return primeiroNumero.ToString("F2", CultureInfo.InvariantCulture);
+        }
+        
+     }
 }
--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,30 +1,134 @@
 ﻿using System;
 using System.Globalization;
+using System.Collections.Generic;
 
 namespace Projeto
 {  
     class Program
     { 
+        public static bool Continuar { get; set; }
+
         public static void Main(String[] args)
         {
+            LigarCalculadora();
+        }
+
+        public static void LigarCalculadora() 
+        {
+            Continuar = true;
             Calculadora calc;
             calc = new Calculadora();
 
-            double[] numeros = { 1.2, 1.3, 1.4, 2.4, 4.4 };
+            while(Continuar)
+            {
+                MenuDeOpcoes(calc);
+            }
+        }
+        public static void MenuDeOpcoes(Calculadora calc)
+        {
+            Console.WriteLine(" _______________________");
+            Console.WriteLine("| Calculadora do Pedrin |");
+            Console.WriteLine("|Somar [1]              |");
+            Console.WriteLine("|Subtrair [2]           |");
+            Console.WriteLine("|Multiplicar [3]        |");
+            Console.WriteLine("|Dividir [4]            |");
+            Console.WriteLine("|Sair [5]               |");
+            Console.WriteLine("|_______________________|");
+            Console.WriteLine("Digite sua opção:      ");
+            int opcao = int.Parse(Console.ReadLine());
 
-            double n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            switch(opcao)
+            {
+                case 1:
+                    Somar(calc);
+                    break;
+                case 2:
+                    Subtrair(calc);
+                    break;
+                case 3:
+                    Multiplicar(calc);
+                    break;
+                case 4:
+                    Dividir(calc);
+                    break;
+                case 5:
+                    Continuar = false;
+                    Console.WriteLine("Obrigado por usar a melhor calculadora do Brasil!");
+                    Console.ReadLine();
+                    break;
+            }
+            
+                    
+        }
 
-            double resultado = calc.Soma(n1, n2);
-            Console.WriteLine("resultado 1 =" + resultado);
+        public static void Somar(Calculadora calc) 
+        {
+            List<double> numerosSoma  = new List<double>{};
 
+            Console.WriteLine("Quantos números serão somados? ");
 
-            double resultado2 = calc.Soma(numeros);
-            Console.WriteLine("resultado 2 =" + resultado2);
+            int qtd = int.Parse(Console.ReadLine());
+            PopularLista(qtd, ref numerosSoma);
 
+            Console.WriteLine("Resultado da soma: "+ calc.Soma(numerosSoma));
+            Console.WriteLine("Aperte qualquer tecla para continuar");
+            Console.ReadLine();
+        }
 
-            Console.Read();
+        public static void Subtrair(Calculadora calc) 
+        {
+            List<double> numerosSubtracao  = new List<double>{};
+
+            Console.WriteLine("Quantos números serão subtraidos? ");
+            int qtd = int.Parse(Console.ReadLine());
+
+            PopularLista(qtd, ref numerosSubtracao);
+
+            Console.WriteLine("Resultado da subtração: "+ calc.Subtrai(numerosSubtracao));
+            Console.WriteLine("Aperte qualquer tecla para continuar");
+            Console.ReadLine();
+        }
+
+        public static void Multiplicar(Calculadora calc) 
+        {
+            List<double> numerosMultiplicacao  = new List<double>{};
+
+            Console.WriteLine("Quantos números serão multiplicados? ");
+
+            int qtd = int.Parse(Console.ReadLine());
+            PopularLista(qtd, ref numerosMultiplicacao);
+
+            Console.WriteLine("Resultado da multiplicação: "+ calc.Multiplica(numerosMultiplicacao));
+            Console.WriteLine("Aperte qualquer tecla para continuar");
+            Console.ReadLine();
+        }
+
+        public static void Dividir(Calculadora calc) 
+        {
+            List<double> numerosDivisao  = new List<double>{};
+
+            Console.WriteLine("Quantos números serão divididos? ");
+
+            int qtd = int.Parse(Console.ReadLine());
+            PopularLista(qtd, ref numerosDivisao);
+
+            Console.WriteLine("Resultado da multiplicação: "+ calc.Divide(numerosDivisao));
+            Console.WriteLine("Aperte qualquer tecla para continuar");
+            Console.ReadLine();
+            
+        }
+
+        public static void PopularLista(int qtd, ref List<double> list) 
+        {
+            for(int i = 0; i < qtd; i ++) 
+            {
+                Console.WriteLine($"Digite o {i + 1}º número: ");
+                list.Add(double.Parse(Console.ReadLine()));
+            }
         }
     }
 
 }
+
+
+
